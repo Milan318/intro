692495f909ad7617461792cdcff1fece380361df
Milan318 <158015797+Milan318@users.noreply.github.com>
2024-02-07 21:46:31 +0530

    Add files via upload

---
+++ lecture 2.1/Q1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    printf("My Name Is Milan Mandaviya");
+    printf("\n My Age Is 23 Years");
+    printf("\n My School Name Is Naranlala School");
+    return 0;
+}
