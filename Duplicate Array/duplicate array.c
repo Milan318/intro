380a6a528b4e3a8ffb3a29973e9b789633bcd9e5
Milan318 <158015797+Milan318@users.noreply.github.com>
2024-02-27 17:58:43 +0530

    Add files via upload

---
+++ Duplicate Array/duplicate array.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+int main(){
+int arr[]={1,2,5,6,6};
+  int n= sizeof(arr)/size of (arr[0]);
+  for (int i=0;i<=4;i++){
+  for (int j=i+1;j<=4;j++){
+    if (arr[i]==arr[j])
+   printf("duplicate element is %d",arr[i]);   
+  }
+  }
+}
