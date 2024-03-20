73838cf197ee8450478d3b0f0725a0d693412e36
Milan318 <158015797+Milan318@users.noreply.github.com>
2024-03-20 16:46:24 +0530

    Add files via upload

---
+++ PR.9 Supervision/Lecture23.1/Q1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+
+FILE*evenfile,*oddfile;
+
+evenfile = fopen("even_file.txt","w");
+if(evenfile == NULL)
+{
+    printf("unable to open the file \n ");
+}
+
+oddfile = fopen("odd_file.txt","w");
+if(oddfile == NULL)
+{
+    printf("unable to open the file \n ");
+}
+
+for(int i=50 ; i<=70 ; i++)
+{
+    if(i%2==0)
+    {
+        fprintf(evenfile,"%d",i);
+        if(i!=70) fprintf(evenfile,", ");
+    }else
+    {
+        fprintf(oddfile,"%d",i);
+        if(i!=69) fprintf(oddfile,", ");
+    }
+}
+
+fclose(evenfile);
+fclose(oddfile);
+
+printf("file is sucessfully created");
+
+    return 0;
+}
---
+++ PR.9 Supervision/Lecture23.1/Q2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+struct students{
+
+    int roll_no;
+    char name[50];
+    int chemistry;
+    int maths;
+    int physics;
+};
+
+float percentage(int total_marks)
+{
+    return (float)total_marks/300*100;
+}
+
+int main(){
+
+struct students students[5];
+
+
+
+for(int i=0;i<5;i++)
+{
+    printf("enter the details of student (%d) \n\n",i+1);
+    printf("Roll_no : ");
+    scanf("%d",&students[i].roll_no);
+
+    printf("Name : ");
+    scanf("%s",&students[i].name);
+
+    printf("Chemistry : ");
+    scanf("%d",&students[i].chemistry);
+
+    printf("Maths : ");
+    scanf("%d",&students[i].maths);
+
+    printf("Physics : ");
+    scanf("%d",&students[i].physics);
+
+}
+
+    printf("\nOutput\n\n");
+
+    for(int i=0;i<5;i++)
+    {
+        printf("%s (%d) \n",students[i].name, students[i].roll_no);
+
+        printf("Chemitry : %d\n",students[i].chemistry);
+
+        printf("Maths : %d\n",students[i].maths);
+
+        printf("Physics : %d\n",students[i].physics);
+
+         int total_marks = students[i].chemistry + students[i].maths + students[i].physics;
+
+        printf("Total = %d/300\n",total_marks);
+
+        printf("Percent = %2f%%\n\n",percentage(total_marks));
+
+    }
+
+    return 0;
+}
